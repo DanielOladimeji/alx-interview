782441d74ddc6d40d352f48fc7b696749753f5f1
DanielOladimeji <danieloladimejiblessing@gmail.com>
2023-12-07 12:41:08 -0800

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,16 +1,10 @@
-#!/usr/bin/python3
-"""
-Prime Game
-"""
-
-
 def isWinner(x, nums):
     def calculate_primes(n):
         primes = [True] * (n + 1)
         primes[0] = primes[1] = False
-        for i in range(2, int(n**0.5) + 1):
+        for i in range(2, int(n ** 0.5) + 1):
             if primes[i]:
-                for j in range(i*i, n + 1, i):
+                for j in range(i * i, n + 1, i):
                     primes[j] = False
         return [num for num in range(n + 1) if primes[num]]
 
